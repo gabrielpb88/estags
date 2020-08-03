2d9b8bba2f5d328b92140bd4416417aa6dcb71bb
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-08-03 11:18:33 -0300

    Fix: Refatorado para utilizar ES6

--- rabbitmq/worker.js
+++ rabbitmq/worker.js
@@ -1,14 +1,14 @@
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function (error0, connection) {
+amqp.connect('amqp://localhost', (error0, connection) => {
   if (error0) {
     throw error0;
   }
-  connection.createChannel(function (error1, channel) {
+  connection.createChannel((error1, channel) => {
     if (error1) {
       throw error1;
     }
-    var queue = 'task_queue';
+    let queue = 'task_queue';
 
     channel.assertQueue(queue, {
       durable: true
@@ -17,10 +17,10 @@ amqp.connect('amqp://localhost', function (error0, connection) {
     channel.prefetch(1)
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-    channel.consume(queue, function (msg) {
-      var secs = msg.content.toString().split('.').length - 1;
+    channel.consume(queue, (msg) => {
+      let secs = msg.content.toString().split('.').length - 1;
       console.log(" [x] Received %s", msg.content.toString());
-      setTimeout(function () {
+      setTimeout(() => {
         console.log(" [x] Done");
         channel.ack(msg)
       }, secs * 1000);
