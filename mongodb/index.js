93a77f473d0b809957f3f7fd2bc28dd809daed3e
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-08-18 17:33:06 -0300

    Adicionado extracao de dados do Mongo

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,7 +1,24 @@
 require('dotenv').config()
+const fs = require('fs')
 const mongoose = require('mongoose')
 const User = require('./schemas/User')
 const Movie = require('./schemas/Movie')
 
 const DB_URL = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Movie.find().then(res => {
+//   const documents = []
+//   res.map(movie => {})
+// }
+// ).catch(err => clg(err))
+
+Movie.find().then(res => {
+
+    let objs = ''
+    res.forEach(movie => {
+        objs += JSON.stringify(movie) + '\n'
+    })
+    fs.writeFileSync('../movie.json', objs)
+    process.exit(0)
+})
