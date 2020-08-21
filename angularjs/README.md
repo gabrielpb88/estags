f08ee3bfe04a06249a67d1b0a0fdc13d2384ae02
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-08-21 14:39:12 -0300

    add: Diretivas - ng-show ng-submit ng-model ng-include

---
+++ angularjs/README.md
@@ -0,0 +1,14 @@
+# AngularJS
+
+Tutorial básico de AngularJS 1.8.0
+
+## Diretivas
+
+### ng-app
+O Angular só consegue manipular elementos que estejam dentro da diretiva ng-app.
+
+### ng-controller
+Utilizado para associar um controller ao elemento.
+
+### ng-src
+TL.DR - Utilizar no lugar 'src' em tags <img>
--- angularjs/script.js
+++ angularjs/script.js
@@ -2,20 +2,29 @@
 
     var app = angular.module('githubViewer', [])
 
-    var MainController = function ($scope, $http) {
+    var MainController = async function ($scope, $http) {
 
         var onUserComplete = function (response) {
             $scope.user = response.data
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        }
+
+        var onRepos = function (response) {
+            $scope.repos = response.data
         }
 
         var onError = function (reason) {
-            $scope.error = "Could not fetch the user"
+            $scope.error = "Could not fetch the data."
         }
 
-        $http.get("https://api.github.com/users/robconery")
-            .then(onUserComplete, onError)
+        $scope.search = function(username){
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError)
+        }
 
-        $scope.message = 'Hello, Angular!';
+        $scope.message = "GitHub Viewer"
+        $scope.repoSortOrder = "stargazers_count"
     }
 
     app.controller('MainController', MainController)
