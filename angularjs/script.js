471711f4f462fd848fb4900524405ecdf84594ae
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-08-25 11:48:55 -0300

    add:

--- angularjs/script.js
+++ angularjs/script.js
@@ -2,7 +2,7 @@
 
     var app = angular.module('githubViewer', [])
 
-    var MainController = async function ($scope, $http) {
+    var MainController = async function ($scope, $http, $interval, $log, $location, $anchorScroll) {
 
         var onUserComplete = function (response) {
             $scope.user = response.data
@@ -12,19 +12,41 @@
 
         var onRepos = function (response) {
             $scope.repos = response.data
+            $location.hash("userDetails")
+            $anchorScroll();
         }
 
         var onError = function (reason) {
             $scope.error = "Could not fetch the data."
         }
 
-        $scope.search = function(username){
-            $http.get("https://api.github.com/users/" + username)
-                .then(onUserComplete, onError)
+        var decrementCountdown = function () {
+            $scope.countdown -= 1
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username)
+            }
         }
 
+        var countdownInterval = null
+        var startCountdown = function () {
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        }
+
+        $scope.search = function (username) {
+            $log.info("Searching for " + username)
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError)
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval)
+                $scope.countdown = null
+            }
+        }
+
+        $scope.username = "angular"
         $scope.message = "GitHub Viewer"
         $scope.repoSortOrder = "stargazers_count"
+        $scope.countdown = 5
+        startCountdown()
     }
 
     app.controller('MainController', MainController)
